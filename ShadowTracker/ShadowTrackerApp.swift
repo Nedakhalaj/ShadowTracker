f329b71649f36052ea5b8a06b4f1b062bb03b2c0
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-04-25 13:33:10 +0200

    Initial Commit

---
+++ ShadowTracker/ShadowTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowTrackerApp.swift
+//  ShadowTracker
+//
+//  Created by neda khalajnejad on 2026-04-25.
+//
+
+import SwiftUI
+
+@main
+struct ShadowTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
